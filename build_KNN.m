4b382cdf270fcfebe20a1e8a870f1922244defc0
John Corbett <jtcorbett17@gmail.com>
2015-12-13 23:10:05 -0500

    added KNN classifiers

---
+++ build_KNN.m
@@ -0,0 +1,10 @@
+%% build_KNN: K-Nearest Neighbors
+function model = build_KNN(data, labels, parameters)
+    if length(parameters) == 0
+        K = 5;
+    else
+        K = parameters(1);
+    end
+
+    model = {K data labels};
+end
