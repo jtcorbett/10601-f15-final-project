1548e20658d498f75e445f70cd9ef4cbbd745b20
John Corbett <jtcorbett17@gmail.com>
2015-12-16 18:04:10 -0500

    add HOG to preprocess

--- preprocess.m
+++ preprocess.m
@@ -1,14 +1,16 @@
-function [preprocessed_data preprocess_params] = preprocess(data, parameters, recreate_parameters)
+function [preprocessed_data preprocess_params] = preprocess(original_data, parameters, recreate_parameters)
     if length(parameters) == 0
         HSV = 1;
         normalize = 1;
         dimensions = 1000;
         whiten = 1;
+        hog = 0;
     else
         HSV = parameters{1};
         normalize = parameters{2};
         dimensions = parameters{3};
         whiten = parameters{4};
+        hog = parameters{5};
     end
 
     REBUILD = length(recreate_parameters) == 0;
@@ -29,21 +31,23 @@ function [preprocessed_data preprocess_params] = preprocess(data, parameters, re
 
     data = double(data);
 
-
-    % size(data)
     if HSV
-      for sample_i=1:size(data, 1)
-        data(sample_i, :) = convert_HSV(data(sample_i, :), {});
+      for sample_i=1:size(original_data, 1)
+        data(sample_i, :) = convert_HSV(original_data(sample_i, :), {});
       end
+    elseif hog ~= 0
+      for sample_i=1:size(original_data, 1)
+        data(sample_i, :) = convert_HOG(original_data(sample_i, :), {hog});
+      end
+    else
+        data = original_data;
     end
 
-    % size(data)
     if REBUILD
       population_mean = mean(data);
     end
     data = double(data) - population_mean;
 
-    % size(data)
     if normalize
         disp 'normalizing'; fflush(stdout);
         if REBUILD
@@ -52,7 +56,6 @@ function [preprocessed_data preprocess_params] = preprocess(data, parameters, re
         data = data ./ population_std;
     end
 
-    % size(data)
     if dimensions > 0
       if REBUILD
           disp 'computing covar mat'; fflush(stdout);
@@ -66,7 +69,6 @@ function [preprocessed_data preprocess_params] = preprocess(data, parameters, re
       data = data * population_U;
     end
 
-    % size(data)
     if whiten
         disp 'Whitening'; fflush(stdout);
         if REBUILD && dimensions > 0
@@ -76,7 +78,6 @@ function [preprocessed_data preprocess_params] = preprocess(data, parameters, re
         data = data ./ population_whiten;
     end
 
-    % size(data)
     preprocessed_data = data;
     preprocess_params = {population_mean population_std population_U population_whiten};
 end
