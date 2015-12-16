bc5eebcdefbd6af03b6d9bae3d6cacde6d512353
John Corbett <jtcorbett17@gmail.com>
2015-12-15 20:17:04 -0500

    better names for GNB variables

--- build_GNB.m
+++ build_GNB.m
@@ -2,38 +2,38 @@ function model = build_GNB(data, labels, parameters)
     offset = 1 - min(labels);
     labels = labels + offset;
 
-    N = length(labels);
-    F = size(data,2);
-    Y = unique(labels);
-    K = max(Y);
+    labels_length = length(labels);
+    num_features = size(data,2);
+    unique_labels = unique(labels);
+    num_labels = length(unique_labels);
 
     % calculate priors
-    prior = zeros(size(Y));
+    prior = zeros(size(unique_labels));
 
-    for label = 1:K
-        prior(label) = sum(labels==label)/N;
+    for label = 1:num_labels
+        prior(label) = sum(labels==label)/labels_length;
     end
 
-    u = zeros(F,K);
-    s = zeros(F,K);
+    mu = zeros(num_features,num_labels);
+    variance_mat = zeros(num_features,num_labels);
 
-    for i = 1:F
-        for j = 1:K
+    for i = 1:num_features
+        for j = 1:num_labels
 
             % calculate mean
-            u(i,j) = 1/sum(labels==j) * sum(data(find(labels==j),i));
+            mu(i,j) = 1/sum(labels==j) * sum(data(find(labels==j),i));
 
             % calculate variance
-            for n = 1:N
+            for n = 1:labels_length
                 if labels(n) == j
-                    s(i,j) = s(i,j) + (data(n,i) - u(i,j))^2;
+                    variance_mat(i,j) = variance_mat(i,j) + (data(n,i) - mu(i,j))^2;
                 end
             end
 
             % normalize variance and convert to std. dev.
-            s(i,j) = sqrt(s(i,j)/sum(labels==j));
+            variance_mat(i,j) = sqrt(variance_mat(i,j)/sum(labels==j));
         end
     end
 
-    model = {offset, prior, u, s};
+    model = {offset, prior, mu, variance_mat};
 end
--- test_GNB.m
+++ test_GNB.m
@@ -1,19 +1,19 @@
 function category = test_GNB(model, features)
     offset = model{1};
     prior = model{2};
-    u = model{3};
-    s = model{4};
+    mu = model{3};
+    variance_mat = model{4};
 
-    F = length(features);
-    K = length(prior);
+    num_features = length(features);
+    num_labels = length(prior);
 
     category = 1;
     max_p = -inf;
 
-    for j = 1:K
+    for j = 1:num_labels
         p = log(prior(j));
-        for i = 1:F
-            p = p + max(log(normpdf(double(features(i)),u(i,j),s(i,j))),-1e250);
+        for i = 1:num_features
+            p = p + max(log(normpdf(double(features(i)),mu(i,j),variance_mat(i,j))),-1e250);
         end
 
         if p > max_p
