0d6e2b9c94ea40c3b31d2dce4bd7f6b4f2cc2258
John Corbett <jtcorbett17@gmail.com>
2015-12-15 19:22:48 -0500

    updated PCA

--- convert_PCA.m
+++ convert_PCA.m
@@ -2,31 +2,37 @@
 function [pca_reduced data U] = convert_PCA(features, feature_parameters)
     if length(feature_parameters) == 0
         dimensions = 100;
-        normalize = false;
-        whiten = false;
-    else
+        normalize = 0;
+        whiten = 0;
+        data = double(features) - mean(features);
+    elseif length(feature_parameters) == 5
         dimensions = feature_parameters{1};
         normalize = feature_parameters{2};
         whiten = feature_parameters{3};
+        population_mean = feature_parameters{4};
+        population_std = feature_parameters{5};
+        data = double(features) - population_mean;
+    else
+        disp 'invalid number of args';
+        return;
     end
 
-    data = double(features) - mean(features);
-
-    disp 'normalizing'; fflush(stdout);
     if normalize
+        disp 'normalizing';
         data = data ./ std(data);
     end
 
-    disp 'computing covar mat'; fflush(stdout);
-    covar = cov(data);
-    [U S V] = svd(covar);
+    disp 'computing covar mat';
+    covar = cov(data)
+    size(data)
+    [U, S, ~] = svd(covar);
 
-    disp 'PCA analysis'; fflush(stdout);
+    disp 'PCA analysis';
     dimensions = min(dimensions,size(U,2));
     pca_reduced = data * U(:,1:dimensions);
 
-    disp 'Whitening'; fflush(stdout);
     if whiten
+        disp 'Whitening';
         pca_reduced = pca_reduced / sqrt(S(:,1:dimensions) + 1.0e-50);
     end
 end
