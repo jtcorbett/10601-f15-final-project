2090a516973060659baa6281816c2846dbfe4f68
jbuckman <jacobbuc@andrew.cmu.edu>
2015-12-16 00:05:28 -0500

    augmentations

--- feedforward.m
+++ feedforward.m
@@ -4,7 +4,7 @@ function [output] = feedforward(inputs, weights, biases)
   z = cell(L, 1);
   activation = cell(L, 1);
 
-  activation{1} = features;
+  activation{1} = inputs;
   for layer_i=2:L
     z{layer_i} = activation{layer_i-1}*weights{layer_i} + biases{layer_i}; %'
     activation{layer_i} = relu(z{layer_i});
